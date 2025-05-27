3c5729f791248c74180152942cec3ab3604ea26a
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-27 11:10:47 -0500

    Título: Añadir tipado y manejar estado en Form
    Contenido:
    - Definidos tipos SearchType y CountryType en src/types/index.ts.
    - Implementado useState para manejar estado en Form.tsx.
    - Tipado aplicado a countries en countries.ts.

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,7 +1,15 @@
 import styles from './Form.module.css'
 import {countries} from "../../data/countries.ts";
+import {useState} from "react";
+import type {SearchType} from "../../types";
 
 const Form = () => {
+
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: '',
+  });
+
   return (
     <form className={styles.form}>
       <div className={styles.field}>
@@ -9,11 +17,16 @@ const Form = () => {
         <input type="text"
                id="city"
                name="city"
+               value={search.city}
+               onChange={(e) => setSearch({...search, city: e.target.value})}
                placeholder={'Ciudad'}/>
       </div>
       <div className={styles.field}>
         <label htmlFor="country">País</label>
-        <select>
+        <select id={'country'}
+                value={search.country}
+                name={'country'}
+                onChange={(e) => setSearch({...search, country: e.target.value})}>
           <option value={''}>--Seleccione un país--</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>{country.name}</option>
