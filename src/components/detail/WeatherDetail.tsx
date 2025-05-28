01e5f59c3fcf606f3f2afcce4d5e0d25375b2a0c
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-27 20:37:36 -0500

    🎨 style: aplicar estilos a WeatherDetail con CSS module
    - Creada e implementada `WeatherDetail.module.css` para aplicar estilos.
    - Añadidas clases para mejorar diseño y legibilidad del componente.

--- src/components/detail/WeatherDetail.tsx
+++ src/components/detail/WeatherDetail.tsx
@@ -1,5 +1,6 @@
 import type {WeatherSchema} from "../../hooks/useWeather.ts";
 import {kelvinToCelsius} from "../../utils";
+import styles from './WeatherDetail.module.css'
 
 type WeatherDetailProps = {
   weather: WeatherSchema
@@ -7,10 +8,10 @@ type WeatherDetailProps = {
 
 const WeatherDetail = ({weather}: WeatherDetailProps) => {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Clima de {weather.name}</h2>
-      <p>{kelvinToCelsius(weather.main.temp)}&deg;C</p>
-      <div>
+      <p className={styles.current}>{kelvinToCelsius(weather.main.temp)}&deg;C</p>
+      <div className={styles.temperatures}>
         <p>Min: <span>{kelvinToCelsius(weather.main.temp_min)}&deg;C</span></p>
         <p>Min: <span>{kelvinToCelsius(weather.main.temp_max)}&deg;C</span></p>
       </div>
